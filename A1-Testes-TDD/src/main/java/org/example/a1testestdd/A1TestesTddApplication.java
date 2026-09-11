53b3899e85b503dfea342b52f34faf3fea255612
Pedro-Veiga18 <pedroveigaovando@gmail.com>
2026-09-11 12:54:41 -0300

    A1

---
+++ A1-Testes-TDD/src/main/java/org/example/a1testestdd/A1TestesTddApplication.java
@@ -0,0 +1,13 @@
+package org.example.a1testestdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A1TestesTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A1TestesTddApplication.class, args);
+    }
+
+}
---
+++ A1-Testes-TDD/src/test/java/org/example/a1testestdd/A1TestesTddApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.a1testestdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A1TestesTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
