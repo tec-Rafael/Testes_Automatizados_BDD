1eaa766038ab066e881dbdf13c3725d80414e439
Pedro-Veiga18 <pedroveigaovando@gmail.com>
2026-09-14 20:16:53 -0300

    Organizacao das pastas finais

--- A1-Testes-TDD/src/test/java/A1/devolucao/livro/DevolucaoTestes.java
+++ A1-Testes-TDD/src/test/java/A1/devolucao/livro/DevolucaoTestes.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class DevolucaoTestes {
